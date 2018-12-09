db90a16c3b23c9f46020969b6b28c4caa496c357
iamdylan <43369528+iamdylan@users.noreply.github.com>
2018-12-09 14:06:39 +0530

    Updated ability to add new tasks

--- Todo-List-Project/assets/js/todos.js
+++ Todo-List-Project/assets/js/todos.js
@@ -1,10 +1,21 @@
-$("li").on("click", function(){
+//Snippet for toggling the strikethrough of complete tasks
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-$("span").on("click", function(event){
+//Delete a task by clicking on X
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+//Handling New task inputs
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();  //grab new todo from input
+		$(this).val("");
+		$("ul").append("<li><span>X</span> " + todoText + "</li>")  //create an li and add to ul
+	}
 });
